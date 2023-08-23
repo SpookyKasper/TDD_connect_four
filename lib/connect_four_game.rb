9fbb502fd952510ea208c4acd366700fb7abf139
Kasper <kasperbak89@pm.me>
2023-08-23 17:18:20 +0200

    REFACTOR

--- lib/connect_four_game.rb
+++ lib/connect_four_game.rb
@@ -1,14 +1,14 @@
-require_relative '../lib/connect_four_board.rb'
-require_relative '../lib/connect_four_player.rb'
+require_relative '../lib/connect_four_board'
+require_relative '../lib/connect_four_player'
 
 class Connect_Four_Game
-  BLACK_SMILEY = "\u263A"
-  WHITE_SMILEY = "\u263B"
+  GREEN_SMILEY = "\e[32m\u263A\e[0m".freeze
+  YELLOW_SMILEY = "\e[33m\u263B\e[0m".freeze
 
-  def initialize(board, player_1, player_2)
+  def initialize(board, player1, player2)
     @board = board
-    @player_1 = player_1
-    @player_2 = player_2
+    @player1 = player1
+    @player2 = player2
     @winner = nil
   end
 
@@ -29,11 +29,11 @@ class Connect_Four_Game
   def game_over_message
     if somebody_won?
       winning_stone = check_for_winning_row || check_for_winning_diagonal || check_for_winning_column
-      @winner = winning_stone == @player_1.color ? @player_1.name : @player_2.name
+      @winner = winning_stone == @player1.color ? @player1.name : @player2.name
       puts
-      puts "\e[93mCongratulations #{@winner} you won the game!\e[0m"
+      puts "\e[91mCongratulations #{@winner} you won the game!\e[0m"
     else
-      puts "\e[93mBravo people! it is a tie!\[0m"
+      puts "\e[91mBravo people! it is a tie!\e[0m"
     end
   end
 
@@ -45,7 +45,7 @@ class Connect_Four_Game
   end
 
   def play_round
-    player = is_playing
+    player = current_player
     pick_column_message(player)
     column_num = player.pick_column_num.to_i
     until @board.column_is_free?(column_num)
@@ -67,19 +67,19 @@ class Connect_Four_Game
 
   def set_up_player_names
     ask_player_one_name_message
-    @player_1.name = get_player_name
+    @player1.name = get_player_name
     ask_player_two_name_message
-    @player_2.name = get_player_name
+    @player2.name = get_player_name
   end
 
   def set_up_colors
     pick_color_message
-    if @player_1.pick_color_num == '1'
-      @player_1.color = BLACK_SMILEY
-      @player_2.color = WHITE_SMILEY
+    if @player1.pick_color_num == '1'
+      @player1.color = GREEN_SMILEY
+      @player2.color = YELLOW_SMILEY
     else
-      @player_1.color = WHITE_SMILEY
-      @player_2.color = BLACK_SMILEY
+      @player1.color = YELLOW_SMILEY
+      @player2.color = GREEN_SMILEY
     end
   end
 
@@ -93,7 +93,7 @@ class Connect_Four_Game
 
   def get_winner_name
     stone = check_for_winning_row || check_for_winning_column || check_for_winning_diagonal
-    stone == @player_1.color ? @player_1.name : @player_2.name
+    stone == @player1.color ? @player1.name : @player2.name
   end
 
   def four_consecutives(array)
@@ -114,8 +114,8 @@ class Connect_Four_Game
     @board.board.flatten.count(player.color)
   end
 
-  def is_playing
-    count_stones(@player_1) == count_stones(@player_2) ? @player_1 : @player_2
+  def current_player
+    count_stones(@player1) == count_stones(@player2) ? @player1 : @player2
   end
 
   def check_for_winning_row
@@ -158,10 +158,10 @@ class Connect_Four_Game
   def pick_color_message
     puts <<~HEREDOC
 
-        \e[93m#{@player_1.name}, which color would you like to pick\e[0m?
+      #{@player1.name}, which color would you like to pick?
 
-        \e[32m[1]\e[0m Black
-        \e[32m[2]\e[0m White
+      \e[32m[1]\e[0m Green
+      \e[32m[2]\e[0m Yellow
 
     HEREDOC
   end
@@ -169,8 +169,8 @@ class Connect_Four_Game
   def pick_column_message(player)
     puts <<~HEREDOC
 
-      \e[93mIt's now your turn #{player.name}! in which column would you like to play your stone?
-      Please pick a column number from 1 to 7\e[0m
+      It's now your turn #{player.name}! in which column would you like to play your stone?
+      Please pick a column number from 1 to 7
 
     HEREDOC
   end
@@ -178,9 +178,9 @@ class Connect_Four_Game
   def ask_player_one_name_message
     puts <<~HEREDOC
 
-    \e[93mHey there connect four enthusiasts! ready for rumble?
-    Please be so kind and agree on who will play first...
-    Now please type the name of the first player \e[0m
+      Hey there connect four enthusiasts! ready for rumble?
+      Please be so kind and agree on who will play first...
+      Now please type the name of the first player:
 
     HEREDOC
   end
@@ -188,7 +188,7 @@ class Connect_Four_Game
   def ask_player_two_name_message
     puts <<~HEREDOC
 
-    \e[93mAnd what about you player two?\e[0m
+      And what about you player two?
 
     HEREDOC
   end
@@ -196,7 +196,7 @@ class Connect_Four_Game
   def print_names_set_message
     puts <<~HEREDOC
 
-    \e[93mCool so #{@player_1.name} and #{@player_2.name} let's get started!\e[0m
+      Cool so #{@player1.name} and #{@player2.name} let's get started!
 
     HEREDOC
   end
@@ -204,9 +204,9 @@ class Connect_Four_Game
   def print_colors_set_messagge
     puts <<~HEREDOC
 
-    \e[93mCool so #{@player_1.name} you'll be playing with the #{@player_1.color} stone
-    Ans #{@player_2.name} you'll be playing with the #{@player_2.color} stone
-    Let's go!\e[0m
+      Cool so #{@player1.name} you'll be playing with the #{@player1.color} stone
+      Ans #{@player2.name} you'll be playing with the #{@player2.color} stone
+      Let's go!
 
     HEREDOC
   end
